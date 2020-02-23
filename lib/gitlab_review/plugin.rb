c036f321cd8e88080c6ab8aa20edf6d319bc2db4
Nazim Suleimanov <galaxies1234@gmail.com>
2020-02-23 22:28:56 +0200

    stable reviewers for branch

--- lib/gitlab_review/plugin.rb
+++ lib/gitlab_review/plugin.rb
@@ -2,16 +2,27 @@ module Danger
   class DangerGitlabReview < Plugin
     attr_accessor :my_attribute
 
+    def new_random(seed)
+      Random.new(Digest::MD5.hexdigest(seed).to_i(16))
+    end
+
+    def random_select(branch_name, reviewers, max_reviewers)
+      randgen = roulette.new_random(branch_name)
+      result = []
+      max_reviewers.times do 
+        result << reviewers[randgen(reviewers.length)]
+      end
+      result
+    end
+
     def random(max_reviewers = 2, user_reviewers = [], user_blacklist = [])
       user_blacklist << gitlab.mr_author
-      reviewers = user_reviewers.select { |k, _| !user_blacklist.include? k }.sample(max_reviewers)
+      possible_reviewers = user_reviewers.select { |k, _| !user_blacklist.include? k }
+      reviewers = random_select(gitlab.mr_json['source_branch'], possible_reviewers, max_reviewers)
 
       if reviewers.count > 0
         reviewers = reviewers.map { |r| '@' + r }
-        
-        result = format('We identified %s to be reviewer%s.',
-                        reviewers.join(', '), reviewers.count > 1 ? 's' : '')
-        
+        result = format('Suggested reviewer%s: %s', reviewers.count > 1 ? 's' : '', reviewers.join(', '))
         markdown result
       end
     end
